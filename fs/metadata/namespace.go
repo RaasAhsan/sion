525b433d61bea8b72429e272d527d4a15d42f103
Raas Ahsan <raas.ahsan@gmail.com>
2022-04-14 01:05:06 -0500

    Flesh out more interfaces for metadata server

--- fs/metadata/namespace.go
+++ fs/metadata/namespace.go
@@ -4,14 +4,18 @@ type ChunkId string
 type Path string
 type Replica string
 
+// TODO: Use a RWMutex to synchronize access to the namespace
 type Namespace struct {
 	files map[Path]File
 }
 
+// File inode
 type File struct {
-	path   Path
-	chunks []Chunk
-	size   uint // TODO: can file size be determined easily?
+	path         Path
+	timeCreated  int64
+	timeModified int64
+	size         uint // TODO: can file size be determined easily?
+	chunks       []Chunk
 }
 
 type Chunk struct {
@@ -27,3 +31,19 @@ const (
 	Unavailable ChunkStatus = iota
 	Available
 )
+
+func GetFile() {
+
+}
+
+func NewFile() {
+
+}
+
+func AddFileChunk() {
+
+}
+
+func CommitChunk() {
+
+}
